7f6219bc6c69e3619c12408575f23e93c0b6b057
Julia <piorek.julia@gmail.com>
2021-05-21 13:48:57 +0200

    mobile view and desktop view

--- script.js
+++ script.js
@@ -12,6 +12,39 @@ function readMore(goToParent) {
     }
 }
 
+function showNav() {
+    const x = document.getElementById("aside");
+    if (x.className === "aside") {
+        x.className += " responsive";
+    } else {
+        x.className = "aside";
+    }
+}
+
+function hideNav() {
+    const element = document.getElementById("aside");
+    element.classList.remove("responsive");
+}
+
+const menuItems = document.querySelectorAll(".nav__list li");
+
+menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener("click", hideNav);
+});
+
+const exitMenu = document.querySelector('.nav_mobile--exit');
+
+exitMenu.addEventListener("click", hideNav);
+
+function showArrow() {
+    if (window.pageYOffset >= 200) {
+        document.getElementById("sticky").style.display = "block";
+    } else document.getElementById("sticky").style.display = "none";
+}
+
+document.addEventListener("scroll", showArrow);
+
+
 // readMoreBtn.innerHTML === 'Więcej'
 
 /*
